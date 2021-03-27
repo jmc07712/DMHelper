3aaa01d54431b8ea96ae0150e595ca465c1145d1
jmc07712 <jmc07712@yahoo.com>
2021-03-27 18:35:56 -0400

    Adding json lib

--- Source/Entry.cpp
+++ Source/Entry.cpp
@@ -3,11 +3,31 @@
 
 #include <spdlog/spdlog.h>
 
+#include <json/json.hpp>
+using json = nlohmann::json;
+
 int main(int argc, char **argv) {
     auto app = Application(argc, argv);
     auto window = MainWindow(1280, 720, "DM Helper");
 
     spdlog::info("Test");
+
+    json j2 = {
+            {"pi", 3.141},
+            {"happy", true},
+            {"name", "Niels"},
+            {"nothing", nullptr},
+            {"answer", {
+                           {"everything", 42}
+                   }},
+            {"list", {1, 0, 2}},
+            {"object", {
+                           {"currency", "USD"},
+                         {"value", 42.99}
+                   }}
+    };
+
+    spdlog::info(j2.dump());
 
     window.show();
 
